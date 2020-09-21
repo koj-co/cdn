ff2ed13b07f201b939127808c1010877ce2107c1
Anand Chowdhary <mail@anandchowdhary.com>
2020-09-21 15:47:03 +0530

    :bento: Update Jitsi overwrites

---
+++ assets/jitsi-overwrites.js
@@ -0,0 +1,10 @@
+function jitsiOverwrites() {
+  var watermark = document.querySelector(".watermark");
+  if (watermark) {
+    watermark.style.backgroundImage = "url(https://kojcdn.com/v1593890003/website-v2/logo-white_rrauqe.svg)"
+    watermark.style.maxHeight = "50px";
+  }
+  document.title = (document.title || "").replace("Jitsi", "Koj");
+}
+
+setInterval(jitsiOverwrites, 1000);
--- assets/meet.js
+++
@@ -1,6 +0,0 @@
-var watermark = document.querySelector(".watermark");
-if (watermark) {
-  watermark.style.backgroundImage = "url(https://kojcdn.com/v1593890003/website-v2/logo-white_rrauqe.svg)"
-  watermark.style.maxHeight = "50px";
-}
-document.title = (document.title || "").replace("Jitsi", "Koj");
