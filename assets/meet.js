8594d2b8c6ecfe690341e3c13f7912efb4bd90fa
Anand Chowdhary <mail@anandchowdhary.com>
2020-09-21 15:27:29 +0530

    :bento: Add meet.js for Koj Meet

---
+++ assets/meet.js
@@ -0,0 +1,6 @@
+var watermark = document.querySelector(".watermark");
+if (watermark) {
+  watermark.style.backgroundImage = "url(https://kojcdn.com/v1593890003/website-v2/logo-white_rrauqe.svg)"
+  watermark.style.maxHeight = "50px";
+}
+document.title = (document.title || "").replace("Jitsi", "Koj");
